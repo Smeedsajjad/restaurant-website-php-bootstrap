2562f86c3662c556ddaddb5ab1bfe81dc0c3caa4
Smeedsajjad <samibissnuse@gmail.com>
2024-10-01 22:46:31 +0500

    pta-ni-kia-hoga

--- admin/assets/js/nav.js
+++ admin/assets/js/nav.js
@@ -1,17 +1,17 @@
-/* Created by Tivotal */
-
-let btn = document.querySelector(".fa-bars");
-let sidebar = document.querySelector(".sidebar");
-
-btn.addEventListener("click", () => {
-  sidebar.classList.toggle("close");
+$(".menu > ul > li").click(function (e) {
+  // Remove active from other items
+  $(this).siblings().removeClass("active");
+  // Toggle active for clicked item
+  $(this).toggleClass("active");
+  // SlideToggle the submenu of the clicked item
+  $(this).find("ul").slideToggle();
+  // SlideUp other submenus
+  $(this).siblings().find("ul").slideUp();
+  // Remove active class from all sub-items
+  $(this).siblings().find("ul").find("li").removeClass("active");
 });
 
-let arrows = document.querySelectorAll(".arrow");
-for (var i = 0; i < arrows.length; i++) {
-  arrows[i].addEventListener("click", (e) => {
-    let arrowParent = e.target.parentElement.parentElement;
-
-    arrowParent.classList.toggle("show");
-  });
-}
+// Toggle sidebar on clicking the menu button
+$(".menu-btn").click(function () {
+  $(".sidebar").toggleClass("active");
+});
