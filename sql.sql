3c20ca07aebf2a8bedb7d97d6272df8e8e508e2b
Muhammad Sameed <samibissnuse@gmail.com>
2024-12-24 16:14:58 +0500

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -68,33 +68,7 @@ CREATE TABLE `billingdetails` (
 --
 -- Dumping data for table `billingdetails`
 --
-
-INSERT INTO `billingdetails` (`id`, `firstName`, `lastName`, `country`, `county`, `address`, `address2`, `city`, `postcode`, `phone`, `email`, `notes`, `created_at`) VALUES
-(3, 'Sameed', 'Sajjad', 'pakistan', '', 'Municipal Colony', 'P-32', 'Jaranwala', '37250', '03278545411', 'sameedsajjad@gmail.com', 'Please! knock at the door.', '2024-11-21 17:27:33'),
-(4, 'Sameed', 'Sajjad', 'pakistan', '', 'Municipal Colony', 'P-32', 'Jaranwala', '37250', '03278545411', 'sameedsajjad@gmail.com', '', '2024-11-21 18:44:15'),
-(5, 'Ahad', 'Shaka', 'pakistan', '', 'Defense , St-9', '', 'Jaranwala ', '37250', '12345678912', 'ahad@gmail.com', 'Kia hal hy bhai ?', '2024-11-21 18:52:08'),
-(6, 'Ahad', 'Shaka', 'pakistan', '', 'Defense , St-9', '', 'Jaranwala', '37250', '12345678912', 'ahad@gmail.com', '', '2024-11-21 18:58:14'),
-(7, 'Sameed', 'Sajjad', 'pakistan', '', 'Municipal Colony', 'P-32', 'Jaranwala', '37250', '03278545411', 'sameedsajjad@gmail.com', '', '2024-12-10 17:07:01'),
-(8, 'Sameed', 'Sajjad', 'pakistan', '', 'Municipal Colony', 'P-32', 'Jaranwala', '37250', '03278545411', 'sameedsajjad@gmail.com', '', '2024-12-10 17:07:01'),
-(9, 'Sameed', 'Sajjad', 'pakistan', '', 'Municipal Colony', 'P-32', 'Jaranwala', '37250', '03278545411', 'sameedsajjad@gmail.com', '', '2024-12-10 17:09:02'),
-(10, 'Sameed', 'Sajjad', 'pakistan', '', 'Municipal Colony', 'P-32', 'Jaranwala', '37250', '03278545411', 'sameedsajjad@gmail.com', '', '2024-12-10 17:09:02'),
-(11, 'Sameed', 'Sajjad', 'pakistan', '', 'Municipal Colony', 'P-32', 'Jaranwala', '37250', '03278545411', 'sameedsajjad@gmail.com', '', '2024-12-10 17:13:29'),
-(12, 'Sameed', 'Sajjad', 'pakistan', '', 'Municipal Colony', 'P-32', 'Jaranwala', '37250', '03278545411', 'sameedsajjad@gmail.com', '', '2024-12-10 17:13:29'),
-(13, 'Ahad', 'Shaka', 'pakistan', '', 'Defense , St-9', '', 'Jaranwala', '37250', '12345678912', 'ahad@gmail.com', '', '2024-12-10 17:14:54'),
-(14, 'Ahad', 'Shaka', 'pakistan', '', 'Defense , St-9', '', 'Jaranwala', '37250', '12345678912', 'ahad@gmail.com', '', '2024-12-10 17:14:55'),
-(15, 'Sameed', 'Sajjad', 'pakistan', '', 'Municipal Colony', 'P-32', 'Jaranwala', '37250', '03278545411', 'sameedsajjad@gmail.com', '', '2024-12-10 17:15:37'),
-(16, 'Sameed', 'Sajjad', 'pakistan', '', 'Municipal Colony', 'P-32', 'Jaranwala', '37250', '03278545411', 'sameedsajjad@gmail.com', '', '2024-12-10 17:15:37'),
-(17, 'Sameed', 'Sajjad', 'pakistan', '', 'Municipal Colony', 'P-32', 'Jaranwala', '37250', '03278545411', 'sameedsajjad@gmail.com', '', '2024-12-10 17:17:04'),
-(18, 'Sameed', 'Sajjad', 'pakistan', '', 'Municipal Colony', 'P-32', 'Jaranwala', '37250', '03278545411', 'sameedsajjad@gmail.com', '', '2024-12-10 17:17:04'),
-(19, 'Sameed', 'Sajjad', 'pakistan', '', 'Municipal Colony', 'P-32', 'Jaranwala', '37250', '03278545411', 'sameedsajjad@gmail.com', '', '2024-12-10 17:18:07'),
-(20, 'Sameed', 'Sajjad', 'pakistan', '', 'Municipal Colony', 'P-32', 'Jaranwala', '37250', '03278545411', 'sameedsajjad@gmail.com', '', '2024-12-10 17:18:07'),
-(21, 'Sameed', 'Sajjad', 'pakistan', '', 'Municipal Colony', 'P-32', 'Jaranwala', '37250', '03278545411', 'sameedsajjad@gmail.com', '', '2024-12-10 17:20:00'),
-(22, 'Sameed', 'Sajjad', 'pakistan', '', 'Municipal Colony', 'P-32', 'Jaranwala', '37250', '03278545411', 'sameedsajjad@gmail.com', '', '2024-12-10 17:20:00'),
-(23, 'Sameed', 'Sajjad', 'pakistan', 'Pakistan ', 'Municipal Colony', 'P-32', 'Jaranwala', '37250', '03278545411', 'sameedsajjad@gmail.com', '', '2024-12-10 17:24:06'),
-(24, 'Sameed', 'Sajjad', 'pakistan', 'Pakistan ', 'Municipal Colony', 'P-32', 'Jaranwala', '37250', '03278545411', 'sameedsajjad@gmail.com', '', '2024-12-10 17:24:06'),
-(25, 'Ahad', 'Shaka', 'pakistan', '', 'Defense , St-9', '', 'Jaranwala', '37250', '12345678912', 'ahad@gmail.com', '', '2024-12-10 17:28:39'),
-(26, 'Ahad', 'Shaka', 'pakistan', '', 'Defense , St-9', '', 'Jaranwala', '37250', '12345678912', 'ahad@gmail.com', '', '2024-12-10 17:28:39');
-
+-- Add here your data
 -- --------------------------------------------------------
 
 --
@@ -174,12 +148,7 @@ CREATE TABLE `orders` (
 -- Dumping data for table `orders`
 --
 
-INSERT INTO `orders` (`id`, `user_id`, `billing_id`, `payment_method`, `order_status`, `total_amount`, `created_at`, `updated_at`) VALUES
-(3, 2, 3, 'on', 'pending', 22.62, '2024-11-21 17:27:33', '2024-11-22 21:52:13'),
-(4, 2, 4, 'on', 'cancelled', 12.18, '2024-11-21 18:44:15', '2024-11-24 23:32:40'),
-(5, 4, 5, 'on', 'completed', 12.18, '2024-11-21 18:52:08', '2024-11-24 23:32:34'),
-(6, 4, 6, 'on', 'completed', 11.20, '2024-11-21 18:58:14', '2024-12-10 23:38:39'),
-(9, 2, 9, 'bank-transfer', 'pending', 12.18, '2024-12-10 17:09:02', '2024-12-10 22:09:02');
+-- Add here your data
 
 -- --------------------------------------------------------
 
@@ -199,13 +168,7 @@ CREATE TABLE `order_items` (
 -- Dumping data for table `order_items`
 --
 
-INSERT INTO `order_items` (`id`, `order_id`, `product_id`, `quantity`, `price`) VALUES
-(1, 3, 10, 1, 11.20),
-(2, 3, 8, 1, 11.42),
-(3, 4, 2, 1, 12.18),
-(4, 5, 2, 1, 12.18),
-(5, 6, 10, 1, 11.20),
-(6, 9, 2, 1, 12.18);
+-- Add here your data
 
 -- --------------------------------------------------------
 
@@ -264,13 +227,7 @@ CREATE TABLE `reviews` (
 -- Dumping data for table `reviews`
 --
 
-INSERT INTO `reviews` (`id`, `product_id`, `name`, `email`, `rating`, `review`, `created_at`) VALUES
-(2, 6, 'Sameed', 'sameedsajjad@gmail.com', 2, 'Testing', '2024-12-08 15:52:53'),
-(3, 6, 'ahad', 'ahad@gmail.com', 3, 'test 2', '2024-12-08 15:53:47'),
-(4, 6, 'chalo', 'test@email.com', 3, 'test 3', '2024-12-08 15:57:12'),
-(5, 6, 'example', 'jhon@example.com', 3, 'test', '2024-12-08 16:34:43'),
-(6, 6, 'jdngfjksgn', 'nfj@kdnfksd', 3, 'fnjsk', '2024-12-08 16:34:57'),
-(7, 6, 'htthjrf', 'hrth@fhb', 1, 'fsgfhrgr rthrt', '2024-12-08 16:35:15');
+-- Add here your data
 
 -- --------------------------------------------------------
 
@@ -293,13 +250,7 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `password`, `email`, `full_name`, `phone`, `created_at`, `updated_at`) VALUES
-(1, 'Rehman', '$2y$10$N1g8LHsR8NWXP80F17wnmO8H8N4/SFNQCOA5Al/KL9ftcLndTuE3S', 'example@gmail.com', 'Rehman Anjum', '78945612312', '2024-11-12 19:47:16', '2024-11-12 19:47:16'),
-(2, 'Sameed', '$2y$10$57m/LfSHfyci7Vw2BE2Esu6APTAyxeljH1JXTc5xgsUQ/hk1fUunO', 'sameedsajjad@gmail.com', 'Sameed Sajjad', '03278545411', '2024-11-12 19:47:50', '2024-11-12 19:47:50'),
-(3, 'nuzba@gmail.com', '$2y$10$fyCCfRft1i1E6i3X/PQZkev4WnmOGBeuGVjCQuGwCO/QgrSjQtjsO', 'nuzba@gmail.com', 'Nuzba Sajjad', '12345678912', '2024-11-12 19:48:06', '2024-11-12 19:48:06'),
-(4, 'Ahad', '$2y$10$S7d2RoQ.VHQ7g2USy9NgYe/m3IOaatk9aujXCzAylKYwDBqf6FMLe', 'ahad@gmail.com', 'Ahad', '12345678912', '2024-11-12 20:17:50', '2024-11-12 20:17:50'),
-(5, 'Bilal', '$2y$10$AsA/GDMLvwwkUrkSFC1SiOwc1CO43bSMnp/dlHb/kmvICZ1xvN10G', 'bilal@gmail.com', 'Bilal Khalid', '12345678912', '2024-11-12 20:24:05', '2024-11-12 20:24:05'),
-(6, 'example', '$2y$10$Obsu1BJtW8FnoDRJQPB6necYidSEvIE2YL7KUitspeziuQIBfY92.', 'example2@gmail.com', 'example', '12345678912', '2024-11-12 20:38:29', '2024-11-12 20:38:29');
+-- Add here your data
 
 -- --------------------------------------------------------
 
